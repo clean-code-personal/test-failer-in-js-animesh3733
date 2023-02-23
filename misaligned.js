2a5fdd3b8d8fa4984af9d6375d645beb59d22183
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-23 13:25:01 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -5,7 +5,7 @@ function print_color_map() {
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            console.log(`${i * minorColors.length + j} | ${majorColors[i]} | ${minorColors[j]}`);
         }
     }
     return majorColors.length * minorColors.length;
