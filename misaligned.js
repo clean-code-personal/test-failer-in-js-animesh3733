df0bc11e344ff63d6b95f9b904bae0d679084415
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-26 12:30:21 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,23 @@
 const {expect} = require('chai')
 
+function index_to_number(i,j){
+    return i * 5 + j;
+}
+
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown"];
+    const minorColors = ["Blue", "Orange", "Green", "Brown","Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i *5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            console.log(`${index_to_number(i,j)} | ${majorColors[i]} | ${minorColors[j]}`);
         }
     }
     return majorColors.length * minorColors.length;
 }
 
+
 result = print_color_map();
+
+expect(index_to_number(0,0)).equals(1));
 expect(result).equals(25);
 console.log('All is well (maybe!)');
