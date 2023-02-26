492d3797eaf9ccf1c6ed1ee7a2d206617f56de7e
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-27 00:16:29 +0530

    Update tshirtsTest.js

--- tshirtsTest.js
+++ tshirtsTest.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const obj = require('./tshirts.js');
 
-expect(obj.size(38)).equals('L');
+expect(obj.size(38)).equals('M');
 expect(obj.size(37)).equals('S');
 expect(obj.size(40)).equals('M');
 expect(obj.size(43)).equals('L');
