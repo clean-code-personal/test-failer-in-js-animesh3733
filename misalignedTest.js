fe2b486d028267cea68f8d04dc2033c1d226b2ac
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-27 00:05:52 +0530

    Update misalignedTest.js

--- misalignedTest.js
+++ misalignedTest.js
@@ -2,6 +2,6 @@ const expect = require('chai');
 
 result = print_color_map();
 
-expect(index_to_number(0,0)).equals('01'));
+expect(index_to_number(0,0)).equals('01');
 expect(result).equals(25);
 console.log('All is well (maybe!)');
