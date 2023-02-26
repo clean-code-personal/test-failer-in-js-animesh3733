b856cc56f791c61777ef1cc14fa3e3fd1cbf1040
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-26 12:24:56 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -9,9 +9,3 @@ function size(cms) {
         return 'L';
     }
 }
-
-expect(size(38)).equals('M');
-expect(size(37)).equals('S');
-expect(size(40)).equals('M');
-expect(size(43)).equals('L');
-console.log('All is well (maybe!)');
