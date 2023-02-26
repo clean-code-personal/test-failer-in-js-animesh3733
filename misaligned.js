24646095f67d8560cbb5b015f85a0bc41f316f8c
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-26 23:44:27 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -2,12 +2,16 @@ function index_to_number(i,j){
     return i * 5 + j;
 }
 
+function format_user_manual(i,j,majorColors,minorColor)) {
+    return '${index_to_number(i,j)} | ${majorColor} | ${minorColor}`
+}
+
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown","Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${index_to_number(i,j)} | ${majorColors[i]} | ${minorColors[j]}`);
+            console.log(format_user_manual(i,j,majorColors[i],minorColors[j]));
         }
     }
     return majorColors.length * minorColors.length;
