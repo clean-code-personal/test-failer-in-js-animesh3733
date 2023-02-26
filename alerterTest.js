6afceb523d3cc8168079efabc7e8cc6c9d5a4bb6
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-27 00:13:50 +0530

    Update alerterTest.js

--- alerterTest.js
+++ alerterTest.js
@@ -1,4 +1,6 @@
-alertInCelcius(400.5);
-alertInCelcius(303.6);
+const obj = require(./alerter.js);
+
+obj.alertInCelcius(400.5);
+obj.alertInCelcius(303.6);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
