042df1ccc2e63a4dd503e7cdcae4df262a30611c
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-26 13:03:09 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -24,7 +24,3 @@ function alertInCelcius(farenheit) {
     }
 }
 
-alertInCelcius(400.5);
-alertInCelcius(303.6);
-console.log(`${alertFailureCount} alerts failed.`);
-console.log('All is well (maybe!)');
