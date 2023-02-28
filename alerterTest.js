0bb424a08edd2fb98f0f3589f3de7bcaa2f5e079
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-28 12:17:02 +0530

    Update alerterTest.js

--- alerterTest.js
+++ alerterTest.js
@@ -1,15 +1,14 @@
-const chai = require('chai');
-const assert = chai.assert;
+const assert = require('chai').assert;
+const { alertFailureCount, alertInCelcius } = require('./alerter.js');
+const { describe, it } = require('mocha'); 
 
 describe('alertInCelcius', function() {
-  it('should increase alertFailureCount when networkAlertSub returns a non-ok response',function() {
-    let alertFailureCount = 0;
-    
+  it('should increase alertFailureCount when networkAlertSub returns a non-ok response', function() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     alertInCelcius(32);
-    
-    assert.equal(alertFailureCount,1);
-    
+
+    assert.equal(alertFailureCount,0); 
     });
 });
+
