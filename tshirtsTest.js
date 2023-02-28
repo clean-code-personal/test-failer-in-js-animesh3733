c02fe8a54e98d46ebc8149831856e757b5b58530
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-02-28 12:32:47 +0530

    Update tshirtsTest.js

--- tshirtsTest.js
+++ tshirtsTest.js
@@ -1,8 +1,22 @@
-const {expect} = require('chai');
+const assert = require('chai').assert;
 const obj = require('./tshirts.js');
 
-expect(obj.size(38)).equals('M');
-expect(obj.size(37)).equals('S');
-expect(obj.size(40)).equals('M');
-expect(obj.size(43)).equals('L');
-console.log('All is well (maybe!)');
+describe('T-shirts', function() {
+  describe('size()', function() {
+    it('should return "M" for size 38', function() {
+      assert.equal(obj.size(38), 'M');
+    });
+
+    it('should return "S" for size 37', function() {
+      assert.equal(obj.size(37), 'S');
+    });
+
+    it('should return "M" for size 40', function() {
+      assert.equal(obj.size(40), 'M');
+    });
+
+    it('should return "L" for size 43', function() {
+      assert.equal(obj.size(43), 'L');
+    });
+  });
+});
