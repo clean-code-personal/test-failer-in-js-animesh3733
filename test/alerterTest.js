f5e092c77de23333fa7bee23c3616b4f65e54407
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-01 09:14:30 +0530

    Update alerterTest.js

--- test/alerterTest.js
+++ test/alerterTest.js
@@ -4,8 +4,8 @@ const { describe, it } = require('mocha');
 
 describe('alertInCelcius', function() {
   it('should increase alertFailureCount when networkAlertSub returns a non-ok response', function() {
-    alertInCelcius(400.5);
     alertInCelcius(303.6);
+    alertInCelcius(400.5);
     alertInCelcius(32);
 
     assert.equal(alertFailureCount,1); 
